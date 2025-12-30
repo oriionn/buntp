3f9aefea1e5a33f092e0b0cf4f9cd449b5e2abe5
Orion <contact@oriondev.fr>
2025-12-30 14:22:59 +0100

    feat: add ttl option feat: make request function in the class to do not make variable repetition

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,51 @@
 import { MessageMode, NTP_EPOCH, type Packet } from "./models";
 import { buildRequest, readPacket } from "./packets";
 
-async function makeRequest(address: string): Promise<Packet> {
-    const req = buildRequest()
-
-    return new Promise(async (resolve, reject) => {
-        const client = await Bun.udpSocket({
-            socket: {
-                data(socket, buf) {
-                    let packet = readPacket(buf);
-                    if (packet.messageMode === MessageMode.RESPONSE) {
-                        socket.close();
-                        return resolve(packet);
-                    }
-                }
-            },
-        });
-
-        try {
-            const lookup = await Bun.dns.lookup(address);
-            if (lookup === undefined || lookup.length === 0) return reject(new Error('Invalid address'));
-            address = lookup[0]!.address;
-        } catch (e) {
-            return reject(new Error('Invalid address'));
-        }
-
-        client.send(req, 123, address);
-        setTimeout(() => reject(new Error("Server timed out")), 5 * 1000)
-    });
-}
-
 export class NTPClient {
     address: string;
+    timeout: number;
 
-    constructor(address: string) {
+    constructor(address: string, ttl: number = 5000) {
         this.address = address;
+        this.timeout = ttl;
+    }
+
+    _makeReq(): Promise<Packet> {
+        const req = buildRequest();
+
+        return new Promise(async (resolve, reject) => {
+            const client = await Bun.udpSocket({
+                socket: {
+                    data(socket, buf) {
+                        let packet = readPacket(buf);
+                        if (packet.messageMode === MessageMode.RESPONSE) {
+                            socket.close();
+                            return resolve(packet);
+                        }
+                    }
+                },
+            });
+
+            try {
+                const lookup = await Bun.dns.lookup(this.address);
+                if (lookup === undefined || lookup.length === 0) return reject(new Error('Invalid address'));
+                this.address = lookup[0]!.address;
+            } catch (e) {
+                return reject(new Error('Invalid address'));
+            }
+
+
+            client.send(req, 123, this.address);
+            setTimeout(() => reject(new Error("Server timed out")), this.timeout)
+        });
     }
 
     async rawSync(): Promise<Packet> {
-        return await makeRequest(this.address);
+        return await this._makeReq();
     }
 
     async sync(): Promise<Date> {
-        let res = await makeRequest(this.address);
+        let res = await this._makeReq();
         let second = res.transmitTimestamp.seconds - Number(NTP_EPOCH);
         let ms = Math.floor((res.transmitTimestamp.fraction * 1000) / Math.pow(2, 32));
         let timestamp = second * 1000 + ms;
